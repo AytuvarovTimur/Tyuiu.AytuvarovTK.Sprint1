261730dbc5219ee1abdab60aada177b5698c3398
dimka <dimka556.dim4@gmail.com>
2025-09-28 00:47:54 +0500

    Task3

---
+++ Tyuiu.AytuvarovTK.Sprint1.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AytuvarovTK.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V13
+    {
+        public double MultiplyOfDigits(double number)
+        {
+            int num = (int)number;
+
+            int digit1 = num / 100;
+            int digit2 = (num / 10) % 10;
+            int digit3 = num % 10;
+
+            double product = digit1 * digit2 * digit3;
+
+            return Math.Round(product, 3);
+        }
+    }
+}
