8d4e62cd50e0a96a2cd476d3b19d0c1fb3bce361
dimka <dimka556.dim4@gmail.com>
2025-09-28 02:06:41 +0500

    Task5

---
+++ Tyuiu.AytuvarovTK.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AytuvarovTK.Sprint1.Task5.V4.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AytuvarovTK.Sprint1.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Сприт #1 | Выполнил: Айтуваров Т. К. | ИСПБ-25-1";
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                                         *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                                                                  *");
+            Console.WriteLine("* Задание #5                                                                                                        *");
+            Console.WriteLine("* Вариант #4                                                                                                        *");
+            Console.WriteLine("* Выполнил: Айтуваров Т. К. | ИСПБ-25-1                                                                             *");
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                          *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                                                              *");
+            Console.WriteLine("* Идет k-я секунда суток. Определить, сколько полных часов (h) прошло к этому моменту (например, h=3, если k=13257).*");
+            Console.WriteLine("*                                                                                                                   *");
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                  *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите количество секунд: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine("Количество часов = " + ds.SecondsToHours(x));
+
+            Console.ReadLine();
+        }
+    }
+}
